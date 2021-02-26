1db325f7aaddd42399c2c21c00f235cfd4bcf0ea
Frank McDermott <fmcdermott4@gmail.com>
2021-02-26 15:38:41 -0500

    added coming soon pictures

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,29 @@
+
 document.getElementById("laserCat").addEventListener("mouseover",function(){
-    document.getElementById('horiseon').style.display="none";
-    document.getElementById("laserCat").style.width="80%"
-}
-);
-document.getElementById("laserCat").addEventListener("mouseout",function(){
-    document.getElementById('horiseon').style.display="block";
-    document.getElementById("laserCat").style.width="40%"
-}
+    document.querySelectorAll('.work').forEach(function(el) {
+        el.style.display = 'none';
+     });
+    
+    document.getElementById('laserCat').style.display="block";
+    document.getElementById("laserCat").style.width="80%";}
 );
 document.getElementById("horiseon").addEventListener("mouseover",function(){
-    document.getElementById('laserCat').style.display="none";
-    document.getElementById("horiseon").style.width="80%"
-}
+    document.querySelectorAll('.work').forEach(function(el) {
+        el.style.display = 'none';
+     });
+    
+    document.getElementById('horiseon').style.display="block";
+    document.getElementById("horiseon").style.width="80%";}
+);
+document.getElementById("laserCat").addEventListener("mouseout",function(){
+    document.querySelectorAll('.work').forEach(function(el) {
+        el.style.display = 'block';
+     });
+    document.getElementById("laserCat").style.width="40%";}
 );
 document.getElementById("horiseon").addEventListener("mouseout",function(){
-    document.getElementById('laserCat').style.display="block";
-    document.getElementById("horiseon").style.width="40%"
-}
+    document.querySelectorAll('.work').forEach(function(el) {
+        el.style.display = 'block';
+     });
+    document.getElementById("horiseon").style.width="40%";}
 );
