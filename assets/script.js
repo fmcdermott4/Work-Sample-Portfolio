b26f8fd1cdf2bb23578d6bdb5ce77d78d481e2f3
Frank McDermott <fmcdermott4@gmail.com>
2021-02-27 12:06:59 -0500

    js added to make navbar appear and disappear

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ document.getElementById("laserCat").addEventListener("mouseover",function(){
      });
     
     document.getElementById('laserCat').style.display="block";
-    document.getElementById("laserCat").style.width="80%";}
+    document.getElementById("laserCat").style.width='90%';}
 );
 document.getElementById("horiseon").addEventListener("mouseover",function(){
     document.querySelectorAll('.work').forEach(function(el) {
@@ -13,7 +13,7 @@ document.getElementById("horiseon").addEventListener("mouseover",function(){
      });
     
     document.getElementById('horiseon').style.display="block";
-    document.getElementById("horiseon").style.width="80%";}
+    document.getElementById("horiseon").style.width='90%';}
 );
 document.getElementById("laserCat").addEventListener("mouseout",function(){
     document.querySelectorAll('.work').forEach(function(el) {
@@ -27,3 +27,11 @@ document.getElementById("horiseon").addEventListener("mouseout",function(){
      });
     document.getElementById("horiseon").style.width="40%";}
 );
+document.addEventListener("mousewheel", function(event){
+    if(event.wheelDelta >= 0){
+    document.getElementById("navbar").style.display="block";
+    }else{
+    document.getElementById("navbar").style.display="none";
+    }
+  })
+//   https://stackoverflow.com/questions/4670834/capturing-the-scroll-down-event
